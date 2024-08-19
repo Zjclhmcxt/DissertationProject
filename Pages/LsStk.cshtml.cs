2a423ce1ef8dbafff4396248b300e55a1bd1b8bd
84074 <84074@4>
2024-08-19 12:49:39 +0100

    添加项目文件。

---
+++ Pages/LsStk.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DP.Pages
+{
+    public class LsStkModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
